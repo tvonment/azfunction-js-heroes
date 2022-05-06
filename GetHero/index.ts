9f7be7fc8ff30eec941990de0119304b7988f951
Thomas <65050898+tvonment@users.noreply.github.com>
2022-05-06 15:32:15 +0000

    connect cosmos

--- GetHero/index.ts
+++ GetHero/index.ts
@@ -1,11 +1,17 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, heroIn): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+
+    context.log(heroIn)
+    let hero = heroIn
+
+    
+    context.bindings.heroOut = hero;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { id: 1, name: "Iron Man", power: "rich" }
+        body: hero
     };
 
 };
